0dfc28eb9178f1a7dd27f237c6c11b544c34d08d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-29 00:06:45 +0200

    Formatting

--- test/trello_lab/test/api.clj
+++ test/trello_lab/test/api.clj
@@ -4,45 +4,36 @@
 
 (fact
   (get-boards) => {:method :get
-                   :uri    "/members/me/boards"})
+                   :uri    "/members/me/boards"}
 
-(fact
   (get-board :id) => {:method :get
-                      :uri    "/boards/:id"})
+                      :uri    "/boards/:id"}
 
-(fact
   (get-cards :id) => {:method :get
-                      :uri    "/boards/:id/cards"})
+                      :uri    "/boards/:id/cards"}
 
-(fact
   (get-card :id) => {:method :get
-                     :uri    "/cards/:id"})
+                     :uri    "/cards/:id"}
 
-(fact
   (lists :id) => {:method :get
-                  :uri    "/boards/:id/lists"})
+                  :uri    "/boards/:id/lists"}
 
-(fact
   (get-list :id) => {:method :get
-                     :uri    "/lists/:id"})
+                     :uri    "/lists/:id"}
 
-(fact
   (add-list {:name    :n
              :idBoard :i}) => {:method :post
                                :uri    "/lists/"
                                :params {:name    :n
-                                        :idBoard :i}})
+                                        :idBoard :i}}
 
-(fact
   (add-card :card-data) => {:method :post
                             :uri    "/cards/"
-                            :params :card-data})
+                            :params :card-data}
 
-(fact
   (list-cards :id) => {:method :get
-                       :uri    "/lists/:id/cards/"})
+                       :uri    "/lists/:id/cards/"}
 
-(fact
   (move-card {:id :id
               :name :n
               :idList :i}) => {:method :put
