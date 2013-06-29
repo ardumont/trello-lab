08c004bbae44a40f99ef92f61f395225dd5ee83a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-29 11:19:21 +0200

    Formatting

--- test/trello_lab/test/query.clj
+++ test/trello_lab/test/query.clj
@@ -8,21 +8,21 @@
 
 (fact
   (execute {:method :get
-           :uri    :some-uri
-           :params :some-params}) => :res
+            :uri    :some-uri
+            :params :some-params}) => :res
   (provided
     (api :get :some-uri :some-params) => :res))
 
 (fact
   (execute {:method :post
-           :uri    :some-uri
-           :params :some-params}) => :res
+            :uri    :some-uri
+            :params :some-params}) => :res
   (provided
     (post :some-uri :some-params) => :res))
 
 (fact
   (execute {:method :put
-           :uri    :some-uri
-           :params :some-params}) => :res
+            :uri    :some-uri
+            :params :some-params}) => :res
   (provided
     (put :some-uri :some-params) => :res))
